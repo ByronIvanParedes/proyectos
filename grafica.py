5917c1c32673975a577d89ab7d57dfee3084a203
22B17-PC\22B <byron.paredes@epn.edu.ec>
2017-05-17 12:47:48 -0500

    primera version

---
+++ grafica.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 17 11:51:59 2017
+
+@author: 22B
+"""
+
+from tkinter import*
+
+root = Tk()
+boton = Button(root, text="Clic Me")
+boton.pack()
+boton.grid()
+
+root.mainloop()
